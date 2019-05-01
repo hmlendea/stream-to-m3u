b1e486d6b6908f14dc819d5ac64557f9bda9c0a9
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-05-02 02:13:32 +0300

    New project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IptvPlaylistFetcher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            return;
+        }
+    }
+}
