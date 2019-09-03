4ece03d9a0a811ead888afbbbe1be8d32e8dfbcf
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-09-03 11:14:59 +0300

    Load input file

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using NuciCLI;
+using NuciDAL.Repositories;
+
+using StreamToM3U.DataAccess.DataObjects;
 using StreamToM3U.Net;
 using StreamToM3U.Service;
 
@@ -9,11 +13,21 @@ namespace StreamToM3U
 {
     public class Program
     {
+        static string[] InputFileOptions = { "-i" };
+
         public static void Main(string[] args)
         {
+            string inputFilePath = null;
+
+            if (CliArgumentsReader.HasOption(InputFileOptions))
+            {
+                inputFilePath = CliArgumentsReader.GetOptionValue(InputFileOptions);
+            }
+
             IServiceProvider serviceProvider = new ServiceCollection()
                 .AddSingleton<IFileDownloader, FileDownloader>()
                 .AddSingleton<IPlaylistUrlRetriever, PlaylistUrlRetriever>()
+                .AddSingleton<IRepository<ChannelStreamEntity>>(x => new CsvRepository<ChannelStreamEntity>(inputFilePath))
                 .BuildServiceProvider();
             
             IPlaylistUrlRetriever urlRetriever = serviceProvider.GetService<IPlaylistUrlRetriever>();
