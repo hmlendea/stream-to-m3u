8b2a42c06b603a7e8c60bf37c0cd9acc93371190
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-12-18 15:50:12 +0200

    Configurable request timeout

--- Configuration/ApplicationSettings.cs
+++ Configuration/ApplicationSettings.cs
@@ -2,6 +2,8 @@ namespace StreamToM3U.Configuration
 {
     public sealed class ApplicationSettings
     {
+        public int RequestTimeout { get; set; }
+        
         public string UserAgent { get; set; }
     }
 }
